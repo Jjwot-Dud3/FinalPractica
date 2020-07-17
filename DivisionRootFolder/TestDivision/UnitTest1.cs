cf7eba3b5b5cca79c4c682c724320c522bb4821c
GABRIEL ALEXANDER SANTANA CONTRERAS <1085855@est.intec.edu.do>
2020-07-16 21:37:05 -0400

    Test

--- DivisionRootFolder/TestDivision/UnitTest1.cs
+++ DivisionRootFolder/TestDivision/UnitTest1.cs
@@ -7,14 +7,46 @@ namespace Tests
         
 
         [Test]
-        public void Test_Taking_Numbers()
+        public static void Test_Taking_Numbers()
         {
             double value1 = 36;
             double value2 = 6;
             double expected = 6;
-            Division division = new Division();
-            Assert.AreEqual(expected, division.DivisionMethod(value1, value2));
+           
+            Assert.AreEqual(expected, DivisionLib.Division.DivisionMethod(value1, value2));
 
         }
+        [Test]
+        public static void Test_DivisionOf55_5_Equals_11()
+        {
+            double value1 = 55;
+            double value2 = 5;
+            double expected = 11;
+           
+            Assert.AreEqual(expected, DivisionLib.Division.DivisionMethod(value1, value2));
+
+        }
+        [Test]
+        public static void Test_DivisionOf124_12_Equals_10p33()
+        {
+            double value1 = 124;
+            double value2 = 12;
+            double expected = 10.333333333333334d;
+           
+            Assert.AreEqual(expected, DivisionLib.Division.DivisionMethod(value1, value2));
+
+        }
+        [Test]
+        public static void Test_DivisionOf124_12_Equals_11()
+        {
+            double value1 = 124;
+            double value2 = 12;
+            double expected = 11;
+           
+            Assert.AreEqual(expected, DivisionLib.Division.DivisionMethod(value1, value2));
+
+        }
+
+        
     }
 }
